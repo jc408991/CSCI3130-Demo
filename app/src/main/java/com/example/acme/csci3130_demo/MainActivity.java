fe6e2ffe35672f23909273ea87567860e9079daf
jc408991 <jc408991@dal.ca>
2018-01-25 21:18:32 -0400

    Committing changes to 3130 project.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private TextView textWindow;
+    private EditText editWindow;
+
+
+    public void CopyText(View a) {
+        Button button1 = (Button) a;
+        textWindow =  findViewById(R.id.demoTextBox);
+        editWindow =  findViewById(R.id.demoEditBox);
+        textWindow.setText(editWindow.getText());
+    }
+
 }
